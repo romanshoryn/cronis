ad96ba6ee13861c3039ac96f718f415fcf399f5c
Roman Shoryn <romanshoryn@gmail.com>
2017-11-30 00:11:22 +0300

    Lecture 3: task 6.

--- lib/cronis/lecture_3/task_6.rb
+++ lib/cronis/lecture_3/task_6.rb
@@ -2,11 +2,23 @@ module Cronis
   module Lecture3
     class Palindrom
       def call(string)
+        array = string.split('')
+
+        val = swap(array, 0, array.size - 1).join('')
+
+        string == val
       end
 
       private
       
-      def detect
+      def swap(array, idx1, idx2)
+        return array if idx1 > idx2
+
+        val = array[idx1]
+        array[idx1] = array[idx2]
+        array[idx2] = val
+
+        swap(array, idx1 + 1, idx2 -  1)
       end
     end
   end
--- spec/cronis/lecture_3/task_6_spec.rb
+++ spec/cronis/lecture_3/task_6_spec.rb
@@ -4,21 +4,25 @@ RSpec.describe Cronis::Lecture3::Palindrom do
   describe '#call' do
     context 'when correct string is passed' do
       before do
-        @service = Cronis::Lecture3::SmartOutput.new
+        @service = Cronis::Lecture3::Palindrom.new
       end
   
       it 'should display value through comas' do
         expect(@service.call('abba')).to eq true
       end
+
+      it 'should display value through comas' do
+        expect(@service.call('rrorr')).to eq true
+      end
     end
 
     context 'when incorrect string is passed' do
       before do
-        @service = Cronis::Lecture3::SmartOutput.new
+        @service = Cronis::Lecture3::Palindrom.new
       end
   
       it 'should display value through comas' do
-        expect(@service.call('roma')).to eq true
+        expect(@service.call('roma')).to eq false
       end
     end
   end
